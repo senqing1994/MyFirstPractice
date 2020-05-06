cdcdcba32b65765f801805a7d5376cff0f98acfb
zhaosenqing <291553302@qq.com>
2020-05-06 18:39:32 +0800

    test0202

--- app/src/main/java/com/example/myfirstpracticeapplication/fragments/HomeFragment.java
+++ app/src/main/java/com/example/myfirstpracticeapplication/fragments/HomeFragment.java
@@ -142,7 +142,4 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 break;
         }
     }
-    //我现在是在分支test01上修改；我现在是在分支test01上修改；我现在是在分支test01上修改
-
-    //我现在是在分支test02上修改；我现在是在分支test02上修改；
 }
