869b91a8989d7694471289797ce9e2f18061120b
senqing <291553302@qq.com>
2020-05-13 13:32:36 +0800

    增加功能模块,选择头像

--- app/src/main/java/com/example/myfirstpracticeapplication/UserInfoActivity.java
+++ app/src/main/java/com/example/myfirstpracticeapplication/UserInfoActivity.java
@@ -1,14 +1,205 @@
 package com.example.myfirstpracticeapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class UserInfoActivity extends AppCompatActivity {
+public class UserInfoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int ALBUM_OK = 3432;
+    private static final int CUT_OK = 0x0013;
+    //用户头像
+    private ImageView mIvUserLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        initView();
     }
+
+    private void initView() {
+        mIvUserLogin = findViewById(R.id.user_logo);
+        mIvUserLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.user_logo:
+                addHeadPortraitDialog();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 选择头像的dialog
+     */
+    private void addHeadPortraitDialog() {
+        final Dialog dialog = new Dialog(this,R.style.dialog);
+        View dialogView = View.inflate(this,R.layout.photo_choose_dialog,null);
+        dialog.setContentView(dialogView);
+
+        //通过window设置效果
+        Window window = dialog.getWindow();
+        //设置动画，固定在底部，宽度全屏
+//        window.setWindowAnimations(R.style.main_menu_animstyle);
+        window.setGravity(Gravity.BOTTOM);
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        //设置取消按钮的点击事件
+        dialogView.findViewById(R.id.user_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        //设置从手机选择照片的按钮点击事件
+        dialogView.findViewById(R.id.image_depot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(ContextCompat.checkSelfPermission(UserInfoActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(UserInfoActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                }else {
+                    openAlbum();
+                }
+                dialog.dismiss();
+            }
+        });
+
+        //显示
+        dialog.show();
+    }
+
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,ALBUM_OK);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else {
+                    Toast.makeText(this,"You denied the permission",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case ALBUM_OK:
+                if(resultCode == RESULT_OK){
+                    if(Build.VERSION.SDK_INT >= 19){
+                        handleImageOnKitKat(data);
+                    }else {
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+//            case CUT_OK:
+                //拿到真正处理后的照片
+//                    Bundle extras = data.getExtras();
+//                    Bitmap bitmap = extras.getParcelable("data");
+//                    mIvUserLogin.setImageBitmap(bitmap);
+//                    break;
+            default:
+                break;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if(DocumentsContract.isDocumentUri(this,uri)){
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }
+        }else if ("content".equalsIgnoreCase(uri.getScheme())){
+            imagePath = getImagePath(uri,null);
+        }else if ("file".equalsIgnoreCase(uri.getScheme())){
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);
+    }
+
+    private void displayImage(String imagePath) {
+        if(imagePath != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            mIvUserLogin.setImageBitmap(bitmap);
+        }else {
+            Toast.makeText(this,"failed to get image",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if(cursor != null){
+            if(cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Toast.makeText(this,"版本低于4.4",Toast.LENGTH_LONG).show();
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+
+//    private void clipImage(Uri uri) {
+//        Intent intent = new Intent("com.android.camera.action.CROP");
+//        intent.setDataAndType(uri,"image/*");
+//        //传递数据
+//        intent.putExtra("crop","true");
+//        startActivityForResult(intent,CUT_OK);
+//    }
+
 }
